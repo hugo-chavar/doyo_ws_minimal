a877d081efac4c0cf5f37f8df3e00b57121f7931
Hugo Chavar <hechavar@gmail.com>
2025-07-20 15:19:38 -0300

    Fix

--- lib/doyo_ws_web/endpoint.ex
+++ lib/doyo_ws_web/endpoint.ex
@@ -15,7 +15,7 @@ defmodule DoyoWsWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
-  socket "/new_ws", WsServerWeb.UserSocket,
+  socket "/new_ws", DoyoWsWeb.UserSocket,
     # to remove /websocket see https://chatgpt.com/share/67e1d7d9-49e4-8000-b3f1-0e1f2fadf226 nginx solution
     websocket: [
       timeout: 600_000, # 10 minutes instead of the default 60 seconds
