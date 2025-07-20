868a98cddea5f580be705881f47223c5682b3a09
Hugo Chavar <hechavar@gmail.com>
2025-07-20 16:38:18 -0300

    Fix redis subscriber error

--- lib/doyo_ws/redis_subscriber.ex
+++ lib/doyo_ws/redis_subscriber.ex
@@ -3,15 +3,17 @@ defmodule DoyoWs.RedisSubscriber do
   require Logger
 
   def start_link(_opts) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
   @impl true
   def init(_) do
+    # Start a dedicated Redix connection for PubSub
     {:ok, conn} = Redix.start_link(name: :redix_pubsub)
 
-    # Subscribe to "orders" channel
+    # Subscribe in the background
     send(self(), :subscribe)
+
     {:ok, %{conn: conn}}
   end
 
@@ -19,6 +21,12 @@ defmodule DoyoWs.RedisSubscriber do
   def handle_info(:subscribe, state) do
     {:ok, _subscription_ref} = Redix.PubSub.subscribe(state.conn, "orders", self())
     Logger.info("Subscribed to Redis channel: orders")
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:redix_pubsub, _conn, :subscribed, %{channel: "orders"}}, state) do
+    Logger.debug("Successfully subscribed to 'orders'")
     {:noreply, state}
   end
 
@@ -32,10 +40,16 @@ defmodule DoyoWs.RedisSubscriber do
         DoyoWsWeb.Endpoint.broadcast(topic, "update", data)
         Logger.info("Broadcasted to #{topic}: #{inspect(data)}")
 
-      {:error, _reason} ->
-        Logger.error("Failed to parse JSON payload: #{payload}")
+      {:error, reason} ->
+        Logger.error("Failed to parse JSON: #{inspect(reason)} - Payload: #{payload}")
     end
 
+    {:noreply, state}
+  end
+
+  # Optional: catch-all
+  def handle_info(msg, state) do
+    Logger.debug("Unhandled message: #{inspect(msg)}")
     {:noreply, state}
   end
 end
