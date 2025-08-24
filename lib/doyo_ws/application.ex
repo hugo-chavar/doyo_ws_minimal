9813e40c9062fcd49c7d318de9769dbf352ff08c
Hugo Chavar <hechavar@gmail.com>
2025-08-24 00:32:09 -0300

    Align redis db with Django

--- lib/doyo_ws/application.ex
+++ lib/doyo_ws/application.ex
@@ -7,18 +7,19 @@ defmodule DoyoWs.Application do
   def start(_type, _args) do
     redis_host = System.get_env("REDIS_HOST") || "redis"
     redis_port = String.to_integer(System.get_env("REDIS_PORT") || "6379")
+    redis_db   = String.to_integer(System.get_env("REDIS_DB") || "1")
 
     children = [
       DoyoWsWeb.Telemetry,
       {DNSCluster, query: Application.get_env(:doyo_ws, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: DoyoWs.PubSub},
       DoyoWsWeb.Endpoint,
-      # Command connection (this one supports child_spec/1)
-      {Redix, [host: redis_host, port: redis_port, name: :redix]},
-      # PubSub connection (manual child spec, since Redix.PubSub lacks child_spec/1)
+      # Command connection
+      {Redix, [host: redis_host, port: redis_port, database: redis_db, name: :redix]},
+      # PubSub connection
       %{
         id: Redix.PubSub,
-        start: {Redix.PubSub, :start_link, [[host: redis_host, port: redis_port, name: :redix_pubsub]]}
+        start: {Redix.PubSub, :start_link, [[host: redis_host, port: redis_port, database: redis_db, name: :redix_pubsub]]}
       },
       DoyoWs.RedisSubscriber
     ]
