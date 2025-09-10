74e937316871a6844549a4876f88d8af8de175e5
Hugo Chavar <hechavar@gmail.com>
2025-09-10 17:43:53 -0300

    Add listener for dev

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule DoyoWs.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
+      listeners: [Phoenix.CodeReloader],
       deps: deps()
     ]
   end
