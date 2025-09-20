09c84453d7d1132c153a398f4880a14839bab6f7
Hugo Chavar <hechavar@gmail.com>
2025-09-20 15:41:02 -0300

    Fix counter

--- lib/doyo_ws/redis_message_router.ex
+++ lib/doyo_ws/redis_message_router.ex
@@ -29,7 +29,7 @@ defmodule DoyoWs.RedisMessageRouter do
   def route("counter_" <> type, payload) do
     {:ok, %{"rid" => restaurant_id}} = Jason.decode(payload)
     Logger.info("Stub: Received #{type}_counter for restaurant #{restaurant_id}")
-    count = DoyoWs.OrderItemCounter.get_counter(restaurant_id, type)
+    {:ok, count} = DoyoWs.OrderItemCounter.get_counter(restaurant_id, type)
     topic = "counter:#{type}:#{restaurant_id}"
     DoyoWsWeb.Endpoint.broadcast(topic, "update", count)
     Logger.info("Broadcasted to #{topic}: #{count}")
