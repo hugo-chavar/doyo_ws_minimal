54c3cc7366f9e2c9d52ee86af4d1726abd02727a
Hugo Chavar <hechavar@gmail.com>
2025-09-27 08:39:43 -0300

    Fix rounding

--- lib/doyo_ws/order_serializer/data_mapper.ex
+++ lib/doyo_ws/order_serializer/data_mapper.ex
@@ -10,18 +10,18 @@ defmodule OrderSerializer.DataMapper do
       order_type: order_data["order_type"],
       timestamp: parse_datetime(order_data["timestamp"]),
       items: Enum.map(order_data["items"] || [], &map_order_item/1),
-      total: order_data["total"] || 0.0,
+      total: (order_data["total"] || 0.0) |> Float.round(2),
       total_items: get_item_count(order_data["items"]),
       no_of_guests: order_data["no_of_guests"],
       completed: order_data["completed"] || false,
       billed: Enum.all?(order_data["items"], fn item ->
         item["completed"] || false or item["deleted"] || false
       end),
-      subtotal: order_data["subtotal"] || 0.0,
-      vat: order_data["vat"] || 0.0,
-      service_fee: order_data["service_fee"] || 0.0,
-      flat_person_fee: order_data["flat_person_fee"] || 0.0,
-      home_delivery_fee: order_data["home_delivery_fee"] || 0.0,
+      subtotal: (order_data["subtotal"] || 0.0) |> Float.round(2),
+      vat: (order_data["vat"] || 0.0) |> Float.round(2),
+      service_fee: (order_data["service_fee"] || 0.0) |> Float.round(2),
+      flat_person_fee: (order_data["flat_person_fee"] || 0.0) |> Float.round(2),
+      home_delivery_fee: (order_data["home_delivery_fee"] || 0.0) |> Float.round(2),
       restaurant: order_data["restaurant"],
       order_counter: order_data["order_counter"],
       latest_order_datetime: parse_datetime(order_data["timestamp"]),
@@ -42,24 +42,24 @@ defmodule OrderSerializer.DataMapper do
       product: map_product(item_data["product"]),
       status: get_item_status(item_data),
       user_order_action_status: item_data["user_order_action_status"],
-      actual_price: item_data["actual_price"] || 0.0 |> Float.round(2),
-      ordered_price: item_data["ordered_price"] || 0.0 |> Float.round(2),
+      actual_price: (item_data["actual_price"] || 0.0) |> Float.round(2),
+      ordered_price: (item_data["ordered_price"] || 0.0) |> Float.round(2),
       completed: item_data["completed"] || false,
       deleted: item_data["deleted"] || false,
       paid: item_data["paid"] || false,
       timestamp: get_item_timestamp(item_data),
       is_new: item_is_new(item_data),
       note: item_data["note"] || "",
-      service_fee: item_data["service_fee"] || 0.0 |> Float.round(2),
-      service_fee_vat: item_data["service_fee_vat"] || 0.0 |> Float.round(2),
-      product_vat: item_data["product_vat"] || 0.0 |> Float.round(2),
-      total_vat: item_data["total_vat"] || 0.0 |> Float.round(2),
-      total_price: item_data["total_price"] || 0.0 |> Float.round(2),
-      price_paid: item_data["price_paid"] || 0.0 |> Float.round(2),
-      price_remaining: item_data["price_remaining"] || 0.0 |> Float.round(2),
-      promo_discount: item_data["promo_discount"] || 0.0 |> Float.round(2),
-      order_discount: item_data["order_discount"] || 0.0 |> Float.round(2),
-      total_discount: item_data["total_discount"] || 0.0 |> Float.round(2),
+      service_fee: (item_data["service_fee"] || 0.0) |> Float.round(2),
+      service_fee_vat: (item_data["service_fee_vat"] || 0.0) |> Float.round(2),
+      product_vat: (item_data["product_vat"] || 0.0) |> Float.round(2),
+      total_vat: (item_data["total_vat"] || 0.0) |> Float.round(2),
+      total_price: (item_data["total_price"] || 0.0) |> Float.round(2),
+      price_paid: (item_data["price_paid"] || 0.0) |> Float.round(2),
+      price_remaining: (item_data["price_remaining"] || 0.0) |> Float.round(2),
+      promo_discount: (item_data["promo_discount"] || 0.0) |> Float.round(2),
+      order_discount: (item_data["order_discount"] || 0.0) |> Float.round(2),
+      total_discount: (item_data["total_discount"] || 0.0) |> Float.round(2),
       tag: item_data["tag"],
       round: item_data["round"],
       order_id: item_data["order_id"],
@@ -130,7 +130,7 @@ defmodule OrderSerializer.DataMapper do
   end
 
   defp get_item_timestamp(item_data) do
-    Logger.info("get_item_timestamp. #{inspect(item_data)}")
+    # Logger.info("get_item_timestamp. #{inspect(item_data)}")
     case item_data do
       %{"user_order_action_status" => %{"current" => %{"timestamp" => ts}}} ->
         parse_datetime(ts)
@@ -158,13 +158,13 @@ defmodule OrderSerializer.DataMapper do
   defp parse_datetime(datetime_str) do
     case DateTime.from_iso8601(datetime_str) do
       {:ok, dt, _offset} ->
-        Logger.info("DateTime.from_iso8601. #{datetime_str}")
+        # Logger.info("DateTime.from_iso8601. #{datetime_str}")
         dt
       {:error, :missing_offset} ->
         case NaiveDateTime.from_iso8601(datetime_str) do
           {:ok, naive_dt} ->
             {:ok, dt} = DateTime.from_naive(naive_dt, "Etc/UTC")
-            Logger.info("DateTime.from_naive. #{datetime_str}")
+            # Logger.info("DateTime.from_naive. #{datetime_str}")
             dt
           error -> error
         end
