322e50355ac667fd85a8132d41029af4b624bdcc
Hugo Chavar <hechavar@gmail.com>
2025-10-16 15:21:26 -0300

    Fix tables service

--- lib/doyo_ws/table_reservation_service.ex
+++ lib/doyo_ws/table_reservation_service.ex
@@ -3,24 +3,24 @@ defmodule DoyoWs.TableReservationService do
   @redis_client Application.compile_env!(:doyo_ws, :redis_impl)
 
   def get_by_restaurant(restaurant_id) do
-    case @redis_client.hvals("table_reservations_#{restaurant_id}") do
-      {:ok, table_reservations_list} ->
-        table_reservations_list
+    case @redis_client.hvals("tables_#{restaurant_id}") do
+      {:ok, tables_list} ->
+        tables_list
         |> Enum.map(&JSON.decode/1)
         |> Enum.filter(fn
-          {:ok, _table_reservations} -> true
+          {:ok, _table} -> true
           {:error, _} -> false
         end)
-        |> Enum.map(fn {:ok, table_reservation} -> table_reservation end)
+        |> Enum.map(fn {:ok, table} -> table end)
 
       {:error, reason} ->
-        Logger.error("Error get table_reservations by restaurant id #{restaurant_id}. Reason: #{reason}")
+        Logger.error("Error get tables by restaurant id #{restaurant_id}. Reason: #{reason}")
     end
   end
 
   def get_by_table(restaurant_id, table_id) do
     get_by_restaurant(restaurant_id)
-    |> Enum.filter(fn reservation -> reservation["table_order"]["id"] == table_id end)
+    |> Enum.filter(fn table -> table["id"] == table_id end)
   end
 
   def get_single_table(restaurant_id, table_id) do
