5cedc0f59475b8781e9305858068b2ad51ec8b23
Hugo Chavar <hechavar@gmail.com>
2025-10-18 09:41:59 -0300

    Fix amount rounding

--- lib/doyo_ws/order_serializer/data_mapper.ex
+++ lib/doyo_ws/order_serializer/data_mapper.ex
@@ -241,7 +241,7 @@ defmodule OrderSerializer.DataMapper do
       %{count: count, amount: amount, items: items} = status_data ->
         Map.put(acc, status, %{
           status_data |
-          amount: amount + item["total_price"],
+          amount: float_round(amount + item["total_price"]),
           count: count + 1,
           items: [item | items]
         })
