59f3c6faf0d20a03ff7cbb0b508768e6b9037134
Hugo Chavar <hechavar@gmail.com>
2025-10-28 10:34:39 -0300

    Allow debug messages in production

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: DoyoWs.Finch
 # Disable Swoosh Local Memory Storage
 config :swoosh, local: false
 
-# Do not print debug messages in production
-config :logger, level: :info
+# Allow debug messages in production
+config :logger, level: :debug
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
